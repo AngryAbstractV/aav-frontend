21f9fd5f950592106706b315be411956c043b963
Daniel Martinez <daniel_ends@yahoo.com>
2022-10-19 15:27:44 -0600

    -using chips and updating dropzone with react dropzone

--- src/UploadImages.js
+++ src/UploadImages.js
@@ -1,6 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import "./style.css"
 import * as tf from "@tensorflow/tfjs";
+import {Backdrop, Chip, CircularProgress, Grid, Stack} from "@mui/material";
+import Dropzone from 'react-dropzone'
+
 
 
 export default function UploadImages() {
@@ -16,14 +19,15 @@ export default function UploadImages() {
         };
         const getClassLabels = async () => {
             const testLabel = ["awe", "anger", "amusement", "contentment", "disgust",
-                "excitement", "fear", "sadness"]
+                "fear", "sadness", "excitement"]
             setClassLabels(testLabel);
         };
         loadModel();
         getClassLabels();
     }, []);
 
-
+    const [currentImage, setImage] = useState(null);
+    const [loading, setLoading] = useState(false);
     const [confidence, setConfidence] = useState(null);
     const [predictedClass, setPredictedClass] = useState(null);
 
@@ -42,70 +46,73 @@ export default function UploadImages() {
     const createHTMLImageElement = (imageSrc) => {
         return new Promise((resolve) => {
             const img = new Image();
+
             img.onload = () => resolve(img);
+
             img.src = imageSrc;
         });
     };
 
 
-    const [images, setImages] = useState([]);
-    const [imageURLs, setImageURLs] = useState([])
-
-
-    useEffect(() => {
-        if (images.length < 1){
-            return
-        }
-        const newImageUrls = [];
-        images.forEach(image => newImageUrls.push(URL.createObjectURL(image)))
-        setImageURLs(newImageUrls)
-    }, [images]);
-
-    async function onImageChange(files) {
-        setImages([...files.target.files]);
-        console.log(files.target.files)
+    const handleImageChange = async (files) => {
         if (files.length === 0) {
-            console.log("where the fuck is my image 1")
             setConfidence(null);
             setPredictedClass(null);
         }
-        if (files.target.files.length === 1) {
-            console.log("where the fuck is my image 2")
-            const imageSrc = await readImageFile(files.target.files[0]);
+        if (files.length === 1) {
+            setImage(files[0])
+            setLoading(true);
+            const imageSrc = await readImageFile(files[0]);
             const image = await createHTMLImageElement(imageSrc);
+            setImage(image)
             const [predictedClass, confidence] = tf.tidy(() => {
                 const tensorImg = tf.browser.fromPixels(image).resizeNearestNeighbor([120, 120]).toFloat().expandDims();
                 const result = model.predict(tensorImg);
-                console.log(result)
                 const predictions = result.dataSync();
-                console.log(predictions)
                 const predicted_index = result.as1D().argMax().dataSync()[0];
                 const predictedClass = classLabels[predicted_index];
                 const confidence = Math.round(predictions[predicted_index] * 100);
-                console.log(confidence)
-
                 return [predictedClass, confidence];
             });
             setPredictedClass(predictedClass);
             setConfidence(confidence);
+            setLoading(false);
         }
-        console.log("where the fuck is my image 3")
-    }
-
-    return(
-        <div className={"center"}>
-            <input className={"center"} type="file" multiple accept={"image/*"} onChange={onImageChange}/>
-            { imageURLs.map(imageSrc => <img className={"photo"} src={imageSrc}  alt={"Current_Image"}/>)}
-            <div>
-                <text>{confidence === null ? "Confidence:" : `Confidence: ${confidence}%`}</text>
-            </div>
-            <div>
-                <text>{predictedClass === null ? "Predicted Emotion:" : `Predicted Emotion: ${predictedClass}`}</text>
-            </div>
-        </div>
+    };
 
 
+    return (
+        <Fragment>
+            <Grid container className="App" direction="column" alignItems="center" justifyContent="center" marginTop="12%">
+                <Grid item>
+                    <h1 style={{ textAlign: "center", marginBottom: "1.5em" }}>Emotion Analyzer</h1>
+                    <Dropzone onDrop={acceptedFiles => handleImageChange(acceptedFiles)}>
+                        {({getRootProps, getInputProps}) => (
+                            <section>
+                                <div {...getRootProps()}>
+                                    <input {...getInputProps()} />
+                                    <p>Let me tell you how to feel</p>
+                                </div>
+                            </section>
+                        )}
+                    </Dropzone>
+                    <Stack style={{ marginTop: "2em", width: "12rem" }} direction="row" spacing={1}>
+                        <Chip
+                            label={predictedClass === null ? "Prediction:" : `Prediction: ${predictedClass}`}
+                            style={{ justifyContent: "left" }}
+                            variant="outlined"
+                        />
+                        <Chip
+                            label={confidence === null ? "Confidence:" : `Confidence: ${confidence}%`}
+                            style={{ justifyContent: "left" }}
+                            variant="outlined"
+                        />
+                    </Stack>
+                </Grid>
+            </Grid>
+            <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={loading}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        </Fragment>
     );
 }
-
-
